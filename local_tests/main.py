65ba5765ab4ee62e8932aa5e3e701e9ae89c4e71
Spencer Newcomb <spencenewcomb08@gmail.com>
2025-08-07 12:22:14 -0700

    update local tests

--- local_tests/main.py
+++ local_tests/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from local-tests!")
+import marimo
+
+__generated_with = "0.14.16"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import requests
+
+    return
 
 
 if __name__ == "__main__":
-    main()
+    app.run()
